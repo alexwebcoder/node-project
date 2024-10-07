6d8ffb4fa977ba148fc6f998938365f3034f5f41
alexwebcoder <alexdeleon@stayup.com>
2024-10-07 15:50:58 -0700

    added modules and people.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 const name = 'yoshi';
 
 console.log(name);
+
+const greet = (name) => {
+    console.log(`hello ${name}`)
+}
+
+greet('johhnie')
+
+// console.log(global)
+
+console.log(__dirname);
+console.log(__filename);
