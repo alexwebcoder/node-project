10d1396f3a07373b87894b0c524d06a467fbab73
alexwebcoder <alexdeleon@stayup.com>
2024-10-20 16:08:46 -0700

    new node app

---
+++ newNodeApp/index.js
@@ -0,0 +1,9 @@
+import { getPosts } from "./postController.js";
+
+console.log(getPosts())
+
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils')
+
+// console.log(`Random: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to Farenheit: ${celciusToFahrenheit(0)}`)
