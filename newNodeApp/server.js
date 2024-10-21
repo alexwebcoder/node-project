0eb0285137ebc5e34b477a2597f0a9ec709735e2
alexwebcoder <alexdeleon@stayup.com>
2024-10-20 22:13:11 -0700

    created router

--- newNodeApp/server.js
+++ newNodeApp/server.js
@@ -2,9 +2,16 @@ import http from 'http';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.statusCode = 404;
-  res.end('<h1>Hey There</h1>');
+ if(req.url === '/') {
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+   res.end('<h1>Home Page</h1>');
+ } else if (req.url === '/about') {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>About Page</h1>');
+ } else {
+  res.writeHead(404, { 'Content-Type': 'text/html' });
+  res.end('<h1>File Not Found</h1>')
+ }
 })
 
 server.listen(PORT, () => {
